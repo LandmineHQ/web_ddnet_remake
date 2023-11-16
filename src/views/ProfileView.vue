179959bc139a8629dcf9919ce58c262446201aa2
landmineHQ <z1216198007@gmail.com>
2023-11-16 12:37:50 +0800

    [Add] view of user profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,17 +1,23 @@
 <template>
     <div class="ddnet-profile">
         <ViewInstructionComponent>{{ $t("sidebar.userProfileItem.title") }}</ViewInstructionComponent>
-        <span>{{ useUserInfoStore().name }} </span>
+        <ProfileCardComponent />
+        <DebugComponent />
     </div>
 </template>
 
 <script setup lang="ts">
 import ViewInstructionComponent from "@/components/ViewInstructionComponent.vue";
-import useUserInfoStore from "@/stores/user";
+import ProfileCardComponent from "@/components/ProfileCardComponent.vue";
+import DebugComponent from "@/components/__debugTools__/DebugComponent.vue";
 </script>
 
 <style lang="scss" scoped>
 .ddnet-profile {
+    width: 100%;
     padding: 20px;
+    gap: 20px;
+    display: flex;
+    flex-direction: column;
 }
 </style>@/stores/user
