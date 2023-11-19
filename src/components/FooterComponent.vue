8b2944a2151c2a07eb6845e7e80e9b25dcc3c1b1
landmineHQ <z1216198007@gmail.com>
2023-11-19 16:36:22 +0800

    [Feature] new component of FooterComponent

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="ddnet-footer">
+        <div class="text" v-for="(item, index) in footer" :key="index">
+            <span class="content">{{ item.content }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const footer = [{ content: "@2023" },
+{ content: "Landmine地雷&shenyang" },
+{ content: "备案号备案号备案号备案号" },
+{ content: "关于" },
+]
+
+</script>
+
+<style scoped lang="scss">
+.ddnet-footer {
+    display: flex;
+    width: 100%;
+    height: 64px;
+    padding: 10px;
+    justify-content: center;
+    align-items: flex-end;
+    gap: 10px;
+
+    background: var(--BG-color-1, #E4E4E4);
+}
+
+.ddnet-footer>.text {
+    >.content {
+        color: var(--Text-1, #000);
+        font-family: Arial;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        opacity: 0.5;
+    }
+}
+</style>
