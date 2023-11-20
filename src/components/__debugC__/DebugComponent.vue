48d2a8b2070539bfa8001ebe2e48d96954e86191
landmineHQ <z1216198007@gmail.com>
2023-11-20 16:39:33 +0800

    [Update] debug component in to tsconfig excluded

--- src/components/__debugC__/DebugComponent.vue
+++ src/components/__debugC__/DebugComponent.vue
@@ -1,8 +1,16 @@
 <template>
     <div>
         here is debugger
+        <div>
+            <button @click="click1">清除indexedDB</button>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-</script>@/stores/UserInfoStore
+import UserIndexedDB from '@/stores/UserIndexedDB';
+
+function click1() {
+    UserIndexedDB.cleanIndexedDB()
+}
+</script>
