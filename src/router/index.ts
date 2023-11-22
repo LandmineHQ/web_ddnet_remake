0a6b6302d7b10e9d62a5034ad238add84b63f9fb
landmineHQ <z1216198007@gmail.com>
2023-11-22 16:55:16 +0800

    [Refact] use hashhistory instand of history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from './routes'
 import useUserInfoStore from '@/stores/user'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
